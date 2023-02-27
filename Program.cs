5079a0c2f11ded19e30083299a2d91f0ce530e1f
g-scope <globally.scoped@yahoo.com>
2023-02-26 21:40:00 -0500

    Planning I guess?...

--- Program.cs
+++ Program.cs
@@ -1,3 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+    Excpected Functions & Endpoints, a story.
 
-Console.WriteLine("BOOM!");
+    Target uilities to use:
+        - MVC
+
+    Device Types:
+        Client - A device that requests data from the Vault.
+        Guard - A device that approves/denies sharing the data.
+        Vault - Host containing data.
+
+    Classes:
+        - Status
+            A class for generic status handling.
+
+            - int Status.Code
+                Following HTTP(s) numbers.
+
+            - string Status.Message
+                Message correlated with Status.Code.
+            
+             - method (int status, string message) get_status()
+                returns first Status.Code, then Status.Message.
+                return (status: 0?, message: ""?)
+            
+            - method void set_status(int status, string message = "")
+                Will update the Status.Code & Status.Message value.
+        
+
+        - ClientRequest
+            Packet of data to Client.
+
+            - class ClientRequest.status
+                container for status data.
+
+
+        - ClientResponse : Status
+            Packet of data from Client.
+
+            - class ClientResponse.status
+                container for status data.
+        
+        - ClientData
+            General data of the Client.
+
+
+*/  
+
