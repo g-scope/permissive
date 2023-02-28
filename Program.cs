09fb6219375862b6f21d6bfffb404b72efa63a3e
g-scope <globally.scoped@yahoo.com>
2023-02-28 13:54:03 -0500

    Changes

--- Program.cs
+++ Program.cs
@@ -1,31 +1,5 @@
 ﻿/*
-    Oops I shouldn't of done the client/status structure!
+    Writing with a better naming scheme and better setup.
+    I want this to be perfection once it's out the gate!
 */  
 
-using Permissive.Data;
-using Permissive.Generator;
-using Permissive.Controller;
-using System.Text;
-
-ExchangeController exchange = ExchangeGenerator.CreateExchangeController();
-
-byte[] data = System.Text.Encoding.ASCII.GetBytes("test message!");
-
-bool success = exchange.publish_data(
-    data: data
-);
-
-Console.WriteLine("Exchange not Authorized yet...");
-Console.WriteLine("Data Published?: " + success);
-Console.WriteLine("Attempted Data Retrieval: " + Encoding.ASCII.GetString(
-    exchange.get_data()
-    )
-);
-
-exchange.authorize_exchange();
-Console.WriteLine("Exchange Authorized!");
-
-Console.WriteLine("Attempted Data Retrieval: " + Encoding.ASCII.GetString(
-    exchange.get_data()
-    )
-);
