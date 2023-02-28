91419fbe7d60e2dfc58256c353bb28a1c2b1e50b
g-scope <globally.scoped@yahoo.com>
2023-02-28 15:23:38 -0500

    Forgot to check if denying works.

--- Program.cs
+++ Program.cs
@@ -12,9 +12,18 @@ exchange.PublishData(
     DefaultData: Encoding.ASCII.GetBytes("I'm a default message!")
 );
 
+// Is not authorized, will return the DefaultData.
 Console.WriteLine("Current Data: " + exchange.GetDataAsString());
 
+// Giving authorization to return the SecretData.
 exchange.GrantAuthorization();
 Console.WriteLine("Authorized!");
 
+// Will give SecretData now that it is authorized.
+Console.WriteLine("Current Data: " + exchange.GetDataAsString());
+
+exchange.DenyAuthorization();
+Console.WriteLine("Deauthorized!");
+
+// No longer authorized, will return the DefaultData.
 Console.WriteLine("Current Data: " + exchange.GetDataAsString());
